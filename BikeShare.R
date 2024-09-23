8a267afc417fe85543462c929e45421372595b6c
Jefferson Harbaugh <jefferson.harbaugh@gmail.com>
2024-09-23 13:30:55 -0600

    here is my message

--- BikeShare.R
+++ BikeShare.R
@@ -7,6 +7,8 @@ install.packages("GGally")
 install.packages("patchwork")
 install.packages("poissonreg")
 install.packages("glmnet")
+install.packages("rpart")
+library(rpart)
 library(glmnet)
 library(poissonreg)
 library(patchwork)
@@ -158,3 +160,117 @@ kaggle_submission <- prediction %>%
   mutate(count=pmax(0, count)) %>% 
   mutate(datetime=as.character(format(datetime))) #prepare data in required format
 vroom_write(x=kaggle_submission, file="./pregmodel.csv", delim=",")
+
+
+#Cross Validation
+mycleandata <- train1 %>% 
+  select(-casual,-registered) %>% 
+  mutate(count=log(count))
+
+mycleandata
+my_recipe <- recipe(count~.,data=mycleandata) %>% 
+  step_mutate(weather=ifelse(weather==4,3,weather)) %>%
+  step_mutate(weather=factor(weather, levels=1:3, labels=c("Sunny","Misty","Rainy"))) %>%
+  step_time(datetime, features="hour") %>%
+  step_date(datetime, features="dow") %>% 
+  step_mutate(datetime_dow=factor(datetime_dow)) %>% 
+  step_mutate(datetime_hour=factor(datetime_hour)) %>% 
+  step_mutate(season=factor(season, levels=1:4, labels=c("Spring","summer","Fall","Winter"))) %>%
+  step_rm(datetime, temp, holiday, workingday) %>% 
+  step_dummy(all_nominal_predictors()) %>% 
+  step_normalize(all_numeric_predictors())
+
+preg_model <- linear_reg(penalty=tune(),
+                         mixture=tune()) %>% 
+  set_engine("glmnet")
+preg_model
+preg_wf <- workflow() %>% 
+  add_recipe(my_recipe) %>% 
+  add_model(preg_model)
+
+grid_of_tuning_params <- grid_regular(penalty(), mixture(), levels = 10)
+grid_of_tuning_params
+folds <- vfold_cv(mycleandata, v = 5,repeats=1)
+folds
+CV_results <- preg_wf %>% 
+  tune_grid(resamples=folds,
+            grid=grid_of_tuning_params,
+            metrics=metric_set(rmse, mae))
+
+collect_metrics(CV_results) %>% 
+  filter(.metric=="rmse") %>% 
+  ggplot(data=., aes(x=penalty, y=mean, color=factor(mixture))) +
+  geom_line()
+
+bestTune <- CV_results %>% 
+  select_best(metric="rmse")
+bestTune
+final_wf <- preg_wf %>% 
+  finalize_workflow(bestTune) %>% 
+  fit(data=mycleandata)
+
+predict <- final_wf %>% 
+  predict(new_data=test1)
+predict <- exp(predict)
+kaggle_submission <- predict %>% 
+  bind_cols(., test1) %>% 
+  select(datetime, .pred) %>% 
+  rename(count=.pred) %>% 
+  mutate(count=pmax(0, count)) %>% 
+  mutate(datetime=as.character(format(datetime))) #prepare data in required format
+vroom_write(x=kaggle_submission, file="./pregmodel22.csv", delim=",")
+
+#regression tree
+mycleandata <- train1 %>% 
+  select(-casual,-registered) %>% 
+  mutate(count=log(count))
+
+my_mod <- decision_tree(tree_depth = tune(),
+                        cost_complexity = tune(),
+                        min_n=tune()) %>% 
+  set_engine("rpart") %>% 
+  set_mode("regression")
+mycleandata$datetime
+my_recipe <- recipe(count~.,data=mycleandata) %>% 
+  step_mutate(weather=ifelse(weather==4,3,weather)) %>%
+  step_mutate(weather=factor(weather, levels=1:3, labels=c("Sunny","Misty","Rainy"))) %>%
+  step_time(datetime, features="hour") %>%
+  step_date(datetime, features="dow") %>% 
+  step_mutate(datetime_dow=factor(datetime_dow)) %>% 
+  step_mutate(season=factor(season, levels=1:4, labels=c("Spring","summer","Fall","Winter"))) %>%
+  step_rm(datetime, temp, holiday, workingday) %>% 
+  step_mutate(datetime_dow=ifelse(datetime_dow=="Sun",1,datetime_dow)) %>% 
+  step_interact(terms = ~ datetime_dow:datetime_hour) %>% 
+  step_dummy(all_nominal_predictors()) %>% 
+  step_normalize(all_numeric_predictors())
+
+my_recipe <- prep(my_recipe)
+testingit <- bake(my_recipe, new_data=mycleandata)
+testingit$datetime_dow_x_datetime_hour
+preg_wf <- workflow() %>% 
+  add_recipe(my_recipe) %>% 
+  add_model(my_mod)
+
+grid_of_tuning_params <- grid_regular(tree_depth(), cost_complexity(), min_n(), levels = 8)
+folds <- vfold_cv(mycleandata, v = 5,repeats=1)
+CV_results <- preg_wf %>% 
+  tune_grid(resamples=folds,
+            grid=grid_of_tuning_params,
+            metrics=metric_set(rmse))
+bestTune <- CV_results %>% 
+  select_best(metric="rmse")
+bestTune
+final_wf <- preg_wf %>% 
+  finalize_workflow(bestTune) %>% 
+  fit(data=mycleandata)
+predict <- final_wf %>% 
+  predict(new_data=test1)
+predict <- exp(predict)
+predict
+kaggle_submission <- predict %>% 
+  bind_cols(., test1) %>% 
+  select(datetime, .pred) %>% 
+  rename(count=.pred) %>% 
+  mutate(count=pmax(0, count)) %>% 
+  mutate(datetime=as.character(format(datetime))) #prepare data in required format
+vroom_write(x=kaggle_submission, file="./treemodel22.csv", delim=",")
